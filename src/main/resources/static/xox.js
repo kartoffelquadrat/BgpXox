6b53c65da55509c473cf5df529fc229a8bd85a1c
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2021-01-04 15:45:06 -0500

    added client side gui update on async board change

--- src/main/resources/static/xox.js
+++ src/main/resources/static/xox.js
@@ -7,7 +7,7 @@ function verifyGameExists() {
                 throw Error(json.error);
             else
                 // ARL-long poll on "board"-resource that causes re-rendering whenever the board changed.
-                observeResource('/Xox/api/games/'+getGameId()+'/board?hash=', renderBoard, markOffline, "");
+                observeResource('/Xox/api/games/' + getGameId() + '/board?hash=', renderBoard, markOffline, "");
         })
         .catch(error => {
             // Seems like the game does not exist:
@@ -30,11 +30,40 @@ function markOffline() {
     alert('connection to server lost');
 }
 
+function xoxBoardUpdate(board)
+{
+    // update board
+    renderBoard(board);
+
+    // update available handlers / visuals
+
+    // update status string
+}
+
 /**
  * Iterates over received board information and draws the x/o symbol to the grid.
  * This function is called by the resource observer.
  */
-function renderBoard(board)
-{
+function renderBoard(board) {
     console.log('Rendering board...');
+
+    // for all cells, if content told by API is not whitespace: set corresponding class (x/o)
+    if (/\S/.test(board.cells[0][0]))
+        document.getElementById('00').classList.add(board.cells[0][0]);
+    if (/\S/.test(board.cells[1][0]))
+        document.getElementById('10').classList.add(board.cells[1][0]);
+    if (/\S/.test(board.cells[2][0]))
+        document.getElementById('20').classList.add(board.cells[2][0]);
+    if (/\S/.test(board.cells[0][1]))
+        document.getElementById('01').classList.add(board.cells[0][1]);
+    if (/\S/.test(board.cells[1][1]))
+        document.getElementById('11').classList.add(board.cells[1][1]);
+    if (/\S/.test(board.cells[2][1]))
+        document.getElementById('21').classList.add(board.cells[2][1]);
+    if (/\S/.test(board.cells[0][2]))
+        document.getElementById('02').classList.add(board.cells[0][2]);
+    if (/\S/.test(board.cells[1][2]))
+        document.getElementById('12').classList.add(board.cells[1][2]);
+    if (/\S/.test(board.cells[2][2]))
+        document.getElementById('22').classList.add(board.cells[2][2]);
 }
